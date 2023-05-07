9d32a3db0cc1c9d57e25557e4f0eea40e060a058
BasBar <basarabartlomiej@gmail.com>
2023-05-07 11:43:45 +0200

    updated skrypt.sh with --logs flag

--- skrypt.sh
+++ skrypt.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-if [ "$1 == "--date ]; then
+if [ "$1" == --date ]; then
 	date
+fi
+
+if [ "$1" == --logs ]; then
+
+	for ((i=1; i<=100; i++)); do
+		filename="log$i.txt"
+		echo "filename: $filename created by: $0 date: $(date)" > $filename
+	done
 fi
