07691afc9a3f00f306e1e769466d9f9c15c4057f
BasBar <basarabartlomiej@gmail.com>
2023-05-07 13:30:50 +0200

    updated help with short tags

--- skrypt.sh
+++ skrypt.sh
@@ -21,11 +21,11 @@ if [ "$1" == --help ] || [ "$1" == -h ]; then
 	echo ""
 	echo "You can use flags:"
 	echo ""
-	echo "--date to display current date"
+	echo "--date or -d to display current date"
 	echo ""
-	echo "--logs xxx where xxx is a number to create xxx number of "
+	echo "--logs xxx or -l xxx where xxx is a number to create xxx number of "
 	echo "log files with names, creation dates, and script name"
 	echo ""
-	echo "--help to see this message screen"
+	echo "--help or -h to see this message screen"
 	exit 0
 fi
