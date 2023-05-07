008daba3db61f58f526ac78365f38b54c5b89c3f
BasBar <basarabartlomiej@gmail.com>
2023-05-07 11:50:37 +0200

    updated skrypt.sh --logs flag so it can take various arguments

--- skrypt.sh
+++ skrypt.sh
@@ -4,10 +4,17 @@ if [ "$1" == --date ]; then
 	date
 fi
 
-if [ "$1" == --logs ]; then
+if [ "$1" == "--logs" ]; then
+	if [ -z "$2" ]; then
+		files=100
+	else
+		files=$2
+	fi
 
-	for ((i=1; i<=100; i++)); do
+	for ((i=1; i<=$files; i++)); do
 		filename="log$i.txt"
 		echo "filename: $filename created by: $0 date: $(date)" > $filename
 	done
+else
+	echo "Invalid argument. Usage $0 [--logs [files]]"
 fi
