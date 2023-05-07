6129dd27c3a365890423bbab21725d4b701b5af5
BasBar <basarabartlomiej@gmail.com>
2023-05-07 12:38:59 +0200

    added flag --help to skrypt.sh, it describes all flags

--- skrypt.sh
+++ skrypt.sh
@@ -18,3 +18,16 @@ if [ "$1" == "--logs" ]; then
 else
 	echo "Invalid argument. Usage $0 [--logs [files]]"
 fi
+
+if [ "$1" == --help ]; then
+	echo ""
+	echo "You can use flags:"
+	echo ""
+	echo "--date to display current date"
+	echo ""
+	echo "--logs xxx where xxx is a number to create xxx number of "
+	echo "log files with names, creation dates, and script name"
+	echo ""
+	echo "--help to see this message screen"
+	exit 0
+fi
