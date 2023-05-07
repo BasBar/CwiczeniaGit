b9fbf26a7e6026c87e98231023f0d7c62aecb70d
BasBar <basarabartlomiej@gmail.com>
2023-05-07 14:23:31 +0200

    added --error tag with shortcut -e.

--- skrypt.sh
+++ skrypt.sh
@@ -17,6 +17,24 @@ if [ "$1" == "--logs" ] || [ "$1" == -l ]; then
 	done
 fi
 
+if [ "$1" == "--error" ] || [ "$1" == -e ]; then
+	if [ -z "$2" ]; then
+		dirname="error100"
+		files=100
+	else 
+		dirname="error$2"
+		files=$2
+	fi
+
+	mkdir -p "$dirname"
+	cd "$dirname"
+	
+	for ((i=1; i<=$files; i++)); do
+		filename="error$i.txt"
+		echo "filename: $filename created by: $0 date: $(date)" > $filename
+	done
+fi
+
 if [ "$1" == --help ] || [ "$1" == -h ]; then
 	echo ""
 	echo "You can use flags:"
@@ -27,5 +45,13 @@ if [ "$1" == --help ] || [ "$1" == -h ]; then
 	echo "log files with names, creation dates, and script name"
 	echo ""
 	echo "--help to see this message screen"
+	exit 0
+fi
+
+if [ "$1" == "--init" ]; then
+	git clone https://github.com/BasBar/CwiczeniaGit.git .
+
+	export PATH="$PATH:$(pwd)/CwiczeniaGit"
+	echo "Repository cloned to $(pwd)/repository and added to PATH."
 	exit 0
 fi
